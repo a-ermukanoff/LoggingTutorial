e9898fc15901392585ed9d20842596875317ff74
Artyom Ermukanoff <a.ermukanov@ez.meteor.ru>
2024-02-12 15:42:52 +0400

    logging.<level>(<message>)

---
+++ main.py
@@ -0,0 +1,7 @@
+import logging
+
+logging.debug("A DEBUG Message")
+logging.info("An INFO")
+logging.warning("A WARNING")  # по умолчанию используется именно этот уровень логирования
+logging.error("An ERROR")
+logging.critical("A message of CRITICAL severity")
