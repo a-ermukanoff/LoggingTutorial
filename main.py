8238b05dd98cf03efcb02fb0bedd39525e535704
Artyom Ermukanoff <a.ermukanov@ez.meteor.ru>
2024-02-13 09:04:33 +0400

    logging.exception(<message>)

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import logging
 
-logging.basicConfig(level=logging.INFO, filename="py_log.log",filemode="w",
-                    format="%(asctime)s %(levelname)s %(message)s")
+logging.basicConfig(
+    level=logging.INFO,
+    filename="py_log.log",
+    filemode="w",
+    format="%(asctime)s %(levelname)s %(message)s",
+)
 
-x = 3
-y = 0
+x_vals = [2, 3, 6, 4, 10]
+y_vals = [5, 7, 10, 0, 1]
 
-logging.info(f"The values of x and y are {x} and {y}.")
-try:
-    x/y
-    logging.info(f"x/y successful with result: {x/y}.")
-except ZeroDivisionError as err:
-    logging.error("ZeroDivisionError",exc_info=True)
+for x_val, y_val in zip(x_vals, y_vals):
+    x, y = x_val, y_val
+    logging.info(f"The values of x and y are {x} and {y}.")
+    try:
+        x / y
+        logging.info(f"x/y successful with result: {x / y}.")
+    except ZeroDivisionError as err:
+        logging.exception("ZeroDivisionError")
