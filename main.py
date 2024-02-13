12527b1d6b21211f36829d1668e29305aa037fcf
Artyom Ermukanoff <ermukanoff.coding@gmail.com>
2024-02-13 21:35:04 +0400

    refactoring: use formatter

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import logging
 
-logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="w",
-                    format="%(asctime)s %(levelname)s %(message)s")
+logging.basicConfig(
+    level=logging.INFO,
+    filename="py_log.log",
+    filemode="w",
+    format="%(asctime)s %(levelname)s %(message)s",
+)
 logging.debug("A DEBUG Message")
 logging.info("An INFO")
-logging.warning("A WARNING")  # по умолчанию используется именно этот уровень логирования
+logging.warning(
+    "A WARNING"
+)  # по умолчанию используется именно этот уровень логирования
 logging.error("An ERROR")
 logging.critical("A message of CRITICAL severity")
