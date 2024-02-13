932135456b82a0699c711855bd6995bb6d975357
Artyom Ermukanoff <a.ermukanov@ez.meteor.ru>
2024-02-13 08:53:48 +0400

    Logging without exception

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import logging
 
-logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="w",
+logging.basicConfig(level=logging.INFO, filename="py_log.log",filemode="w",
                     format="%(asctime)s %(levelname)s %(message)s")
-logging.debug("A DEBUG Message")
-logging.info("An INFO")
-logging.warning("A WARNING")  # по умолчанию используется именно этот уровень логирования
-logging.error("An ERROR")
-logging.critical("A message of CRITICAL severity")
+
+x = 3
+y = 4
+
+logging.info(f"The values of x and y are {x} and {y}.")
+try:
+    x/y
+    logging.info(f"x/y successful with result: {x/y}.")
+except ZeroDivisionError as err:
+    logging.error("ZeroDivisionError",exc_info=True)
